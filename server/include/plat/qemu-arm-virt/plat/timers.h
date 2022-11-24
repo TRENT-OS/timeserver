f3c6842fce787b2c63a6e81686e40016b6394a20
Axel Heider <axel.heider@hensoldt.net>
2022-11-24 14:19:51 +0100

    cleanup qemu-arm-virt

--- server/include/plat/qemu-arm-virt/plat/timers.h
+++ server/include/plat/qemu-arm-virt/plat/timers.h
@@ -1,30 +1,30 @@
 /*
- * Copyright 2019, Data61
- * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
- * ABN 41 687 119 230.
+ * Copyright 2019, Data61, CSIRO (ABN 41 687 119 230)
  *
- * This software may be distributed and modified according to the terms of
- * the BSD 2-Clause license. Note that NO WARRANTY is provided.
- * See "LICENSE_BSD2.txt" for details.
- *
- * @TAG(DATA61_BSD)
+ * SPDX-License-Identifier: BSD-2-Clause
  */
+
 #pragma once
 
-#define HARDWARE_TIMER_COMPONENT                                       \
-    component HWTimer {                                                \
-        hardware;                                                      \
-        emits TimerIRQ irq;                                          \
+#define HARDWARE_TIMER_COMPONENT \
+    component HWTimer { \
+        hardware; \
+        emits TimerIRQ irq; \
     }
 
-#define HARDWARE_TIMER_INTERFACES                                      \
+#define HARDWARE_TIMER_INTERFACES \
     consumes TimerIRQ hwtimer_irq;
+
+/* no attributes */
 #define HARDWARE_TIMER_ATTRIBUTES
-#define HARDWARE_TIMER_COMPOSITION                                     \
-        component HWTimer hwtimer;                                    \
-        connection seL4HardwareInterrupt hwtimer_irq(from hwtimer.irq, \
-                                                      to hwtimer_irq);
-#define HARDWARE_TIMER_CONFIG                                            \
-    hwtimer.irq_irq_number = 30;                                        \
+
+#define HARDWARE_TIMER_COMPOSITION \
+        component HWTimer hwtimer; \
+        connection seL4HardwareInterrupt hwtimer_irq(from hwtimer.irq, to hwtimer_irq);
+
+#define HARDWARE_TIMER_CONFIG \
+    hwtimer.irq_irq_number = 30; \
     hwtimer.irq_irq_type = "arm";
+
+/* no platform interfaces */
 #define HARDWARE_TIMER_PLAT_INTERFACES
